def9822b55c0427c156c150e50bb54d892a68d24
Dilip Lilaramani <dilip.ajm@gmail.com>
2017-12-14 20:34:21 +0530

    studypal chatterbot

--- api_chatbot.py
+++ api_chatbot.py
@@ -13,6 +13,7 @@ from chatterbot.trainers import ChatterBotCorpusTrainer
 import pandas as pd
 import numpy as np
 import datetime
+import csv
 
 app = Flask(__name__) # create a Flask app
 
@@ -21,27 +22,31 @@ global mybot
 def print_timestamp(step='start'):
 	print('-----------Step: ',datetime.datetime.now())
 
-def get_chat_model(train=False):
+def get_chat_model(train=False, db='sqlite', dbname='./chatter_database.sqlite3'):
 	print_timestamp('get_chat_model start')
+
+	if db == 'sqlite':
+		storage_adapter = 'chatterbot.storage.SQLStorageAdapter'
+	else:
+		storage_adapter = "chatterbot.storage.MongoDatabaseAdapter"
+
 	mybot = ChatBot(
 	"Chat Bot",
-	storage_adapter="chatterbot.storage.MongoDatabaseAdapter",
-	database='chatterbot-database',
-	#database='chatterbot-database-temp',
-	#storage_adapter='chatterbot.storage.SQLStorageAdapter',
-    #database='./database.sqlite3',
-	# logic_adapters=[
-	# 	{
-	# 		"import_path": "chatterbot.logic.BestMatch",
-	# 		"statement_comparison_function": "chatterbot.comparisons.levenshtein_distance",
-	# 		"response_selection_method": "chatterbot.response_selection.get_first_response"
-	# 	}
-	# ]
+	storage_adapter=storage_adapter,
+	database=dbname,
+	logic_adapters=[
+		{
+			"import_path": "chatterbot.logic.BestMatch",
+			"statement_comparison_function": "chatterbot.comparisons.levenshtein_distance",
+			#"response_selection_method": "chatterbot.response_selection.get_first_response"
+		}
+	]
 	)
 
 	if (train):
-		mybot = set_corpus_english(mybot)
-		mybot = set_corpus_movie(mybot)
+		mybot = set_studypal_training(mybot)
+		#mybot = set_corpus_english(mybot)
+		#mybot = set_corpus_movie(mybot)
 	
 	print_timestamp('get_chat_model end')
 	return mybot
@@ -86,17 +91,49 @@ def set_corpus_movie(temp_bot):
 	print_timestamp('set_corpus_movie end')
 	return temp_bot;
 
+def set_studypal_training(temp_bot):
+	print_timestamp('set_studypal_training start')
+	filename = 'data/studypal_messages_text.csv'
+
+	rows = []
+	frame_header = ['message']
+
+	with open(filename, 'rb') as f_input:
+		for row in f_input:
+			rows.append(row)
+			print 'line: '+row
+
+	frame = pd.DataFrame(rows, columns=frame_header)
+	print(frame.head())
+	print('---------------------------------------')
+	
+	arr = frame.iloc[:,:].values
+	arr2 = []
+	
+	for index, str2 in enumerate(arr):
+		str2 = str(str2[0])
+		print 'Index line ',index,' : ', str2
+		arr2.append(str2)
+
+	print('training start')
+	#print(arr2)
+	temp_bot.set_trainer(ListTrainer)
+	temp_bot.train(arr2)
+	print('training finish')
+	print_timestamp('set_studypal_training end')
+	return temp_bot;
+
 @app.after_request
 def add_header(r):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also to cache the rendered page for 10 minutes.
-    """
-    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    r.headers["Pragma"] = "no-cache"
-    r.headers["Expires"] = "0"
-    r.headers['Cache-Control'] = 'public, max-age=0'
-    return r
+	"""
+	Add headers to both force latest IE rendering engine or Chrome Frame,
+	and also to cache the rendered page for 10 minutes.
+	"""
+	r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+	r.headers["Pragma"] = "no-cache"
+	r.headers["Expires"] = "0"
+	r.headers['Cache-Control'] = 'public, max-age=0'
+	return r
 
 @app.route('/get_response/<path:chat>', methods=['POST'])
 def get_response(chat):
@@ -122,8 +159,11 @@ def index():
 
 if __name__ == '__main__':
   print 'initialize chat model...'
-  mybot = get_chat_model()
-  mybot.trainer.export_for_training('data/movie_dialog_export.json')
+
+  #mybot = get_chat_model(train=True) #Sqlite
+  mybot = get_chat_model(train=True, db='mongodb', dbname='studypal_messages')
+
+  #mybot.trainer.export_for_training('data/movie_dialog_export.json')
   print 'Chat model loaded...'
   app.run(debug=False, host='0.0.0.0',port=8000) # this will start a local server
 
